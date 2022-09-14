3ed808112eb270add118a875156914a6b0637905
LeejH601 <77761502+LeejH601@users.noreply.github.com>
2022-09-14 17:26:04 +0900

    N-queen 구현
    문제해결 가능 수준이나 속도가 너무 느린 상태

--- N-Queen.py
+++ N-Queen.py
@@ -10,9 +10,9 @@ class State:
         self.moves = moves
 
     def get_new_Board(self, x, y, moves):
-        board = copy.copy(self.board)
+        board = copy.deepcopy(self.board)
         board[x][y] = 1
-        pairs = self.pairs.copy()
+        pairs = copy.deepcopy(self.pairs)
         pairs.append([x,y])
         return State(board, pairs, self.count-1, moves)
 
@@ -20,17 +20,20 @@ class State:
         result = []
         impossible_line = []
         impossible_low = []
-        for i in range(8):
+        for i in range(len(self.board)):
             if 1 in self.board[i]:
                 impossible_line.append(i)
                 impossible_low.append(self.board[i].index(1))
                 continue
-        for i in range(8):
+        for i in range(len(self.board)):
             if i in impossible_line:
                 continue
-            for j in range(8):
+            for j in range(len(self.board)):
                 if j in impossible_low:
                     continue
+                for pair in self.pairs:
+                    if abs(i - pair[0]) == abs(j - pair[1]):
+                        continue
                 result.append(self.get_new_Board(i,j,moves))
         return result
 
@@ -38,37 +41,44 @@ class State:
         return self.h() + self.g()
 
     def h(self):
-        for i in range(len(self.pairs)):
-            for j in range(len(self.pairs[i:])):
-                sub = [self.pairs[i][0] - self.pairs[i+j][0],self.pairs[i][1] - self.pairs[i+j][1]]
-                if abs(sub[0]) != abs(sub[1]):
-                    return float("inf")
+        # for i in range(len(self.pairs)):
+        #     for j in range(len(self.pairs[i:])):
+        #         sub = [self.pairs[i][0] - self.pairs[i+j][0],self.pairs[i][1] - self.pairs[i+j][1]]
+        #         if sub[0] != sub[1]:
+        #             return float('inf')
+        for pair1 in self.pairs:
+            for pair2 in self.pairs:
+                if pair1 != pair2:
+                    if abs(pair1[0] - pair2[0]) == abs(pair1[1] - pair2[1]):
+                        return float('inf')
         return self.count
 
     def g(self):
-        return self.moves
+        return -self.moves
 
     def __lt__(self, other):
         return self.f() < other.f()
 
     def __str__(self):
-        return "------------------ f(n)=" + str(self.f()) +"\n"+\
+        text = "------------------ f(n)=" + str(self.f()) +"\n"+\
                 "------------------ h(n)=" + str(self.h()) +"\n"+\
-                "------------------ g(n)=" + str(self.g()) +"\n"+\
-            str(self.board[0]) +"\n"+\
-            str(self.board[1]) +"\n"+\
-            str(self.board[2]) +"\n"+\
-            str(self.board[3]) +"\n"+\
-            str(self.board[4]) +"\n"+\
-            str(self.board[5]) +"\n"+\
-            str(self.board[6]) +"\n"+\
-            str(self.board[7]) +"\n"+\
-            "-----------------------"
+                "------------------ g(n)=" + str(self.g()) +"\n"
 
-count = 8
+        for line in self.board:
+            text = text + str(line) + '\n'
+
+        text = text + "-----------------------"
+
+        return text
+
+
+
+
+count = input()
 moves = 0
+
 open_queue = queue.PriorityQueue()
-open_queue.put(State([[0 for _ in range(8)] for _ in range(8)], [], count))
+open_queue.put(State([[0 for _ in range(int(count))] for _ in range(int(count))], [], int(count)))
 
 closed_queue = []
 
@@ -80,6 +90,8 @@ while open_queue:
         break
     moves = current.moves + 1
     for state in current.expand(moves):
-        if state not in closed_queue:
-            open_queue.put(state)
-        closed_queue.append(current)
+        for state2 in closed_queue:
+            if state2.board == state.board:
+                continue
+        open_queue.put(state)
+    closed_queue.append(current)
